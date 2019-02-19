cf716bdb16a776772f2e9f27b060a755ee43631f
georgiasalanti <georgia.salanti@gmail.com>
2019-02-19 17:10:05 +0100

    first commit

---
+++ DL script JC.R
@@ -0,0 +1,46 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(eval = FALSE, echo = TRUE, message = FALSE, warning = FALSE)
+
+## ------------------------------------------------------------------------
+library(readxl)
+library(meta)
+
+## ------------------------------------------------------------------------
+settings.meta(digits = 2)
+
+## ------------------------------------------------------------------------
+AcuteManiaP = read_excel("AcuteManiaP.xls")
+AcuteManiaP = as.data.frame(AcuteManiaP)
+
+## ------------------------------------------------------------------------
+str(AcuteManiaP)
+
+
+
+## ------------------------------------------------------------------------
+pooledOR = metabin(event2, n2, event1, n1,
+                   data = AcuteManiaP, studlab = studlab, sm = "OR",method = "I")
+summary(pooledOR)
+
+## ------------------------------------------------------------------------
+summary(update(pooledOR, sm = "RR"))
+summary(update(pooledOR, sm = "RD"))
+
+## ------------------------------------------------------------------------
+forest(pooledOR, sortvar = n1 + n2 , layout = "JAMA", allstudies = F,
+       fs.study = 10, ff.study = "italic",
+       col.diamond = "green", col.diamond.lines = "red",
+       col.square = "yellow", col.square.lines = "blue", prediction=T)
+
+## ------------------------------------------------------------------------
+pooledORAE = metabin(AE2, n2, AE1, n1, data = AcuteManiaP,
+                     studlab = studlab, sm = "OR",
+                     subset = treat2 == "Haloperidol", method = "I")
+summary(pooledORAE)
+
+## ------------------------------------------------------------------------
+
+
+## ------------------------------------------------------------------------
+forest(pooledORAE, layout = "JAMA")
+
